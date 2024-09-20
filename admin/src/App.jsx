8a81280f557baa3c5863c4ac92064b8ac6c791af
Jeewapadma <udithjeewapadma117@gmail.com>
2024-09-20 15:38:33 +0530

    setup routes for each page

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
+import { Route, Routes } from'react-router-dom'
+import AddItems from './pages/Add/AddItems'
+import ListItems from './pages/List/ListItems'
+import Orders from './pages/Orders/Orders'
 
 const App = () => {
   return (
@@ -9,6 +13,11 @@ const App = () => {
       <hr />
       <div className="app-content">
         <Sidebar />
+        <Routes>
+          <Route path='/add' element={<AddItems/>} />
+          <Route path='/list' element={<ListItems/>} />
+          <Route path='/orders' element={<Orders/>} />
+        </Routes>
       </div>
     </div>
   )
---
+++ admin/src/pages/Add/AddItems.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './AddItems.css'
+
+const AddItems = () => {
+  return (
+    <div>AddItems</div>
+  )
+}
+
+export default AddItems
