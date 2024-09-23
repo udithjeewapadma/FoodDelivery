85a57db4c949d54828bf54153020ccf8a2e2d23b
Jeewapadma <udithjeewapadma117@gmail.com>
2024-09-23 12:08:50 +0530

    Implementing register user functionalities

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import { connectDB } from "./config/db.js";
 import foodRouter from "./routes/foodRoute.js";
+import userRouter from "./routes/userRoute.js";
+import'dotenv/config.js'
 
 //app config
 const app = express()
@@ -16,7 +18,8 @@ connectDB();
 
 //api endpoint
 app.use("/api/food", foodRouter);
-app.use("/images", express.static('uploads'))
+app.use("/images", express.static('uploads'));
+app.use('/api/user', userRouter)
 
 app.get("/", (req,res)=>{
     res.send("API working")
