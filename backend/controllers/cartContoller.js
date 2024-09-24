f05a4e99a290027bf8177b32931f0a73785c2193
Jeewapadma <udithjeewapadma117@gmail.com>
2024-09-24 16:58:14 +0530

    Implementing cart route and controller and implementing add to cart functionality

---
+++ backend/controllers/cartContoller.js
@@ -0,0 +1,32 @@
+import userModel from "../models/userModel.js";
+
+//add items to the cart
+const addToCart = async (req, res) => {
+    try {
+        let userData = await userModel.findOne({_id:req.body.userId});
+        let cartData = await userData.cartData;
+        if(!cartData[req.body.itemId]){
+            cartData[req.body.itemId] = 1
+        }
+        else{
+            cartData[req.body.itemId] += 1
+        }
+        await userModel.findByIdAndUpdate(req.body.userId, {cartData});
+        res.json({success:true, message:"item added to cart"})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false, message:"error"})
+    }
+};
+
+//remove items from the cart
+const removeFromCart = async (req, res) => {
+
+};
+
+//fetch the cart items
+const getCart = async (req, res) => {
+
+};
+
+export { addToCart, removeFromCart, getCart };
